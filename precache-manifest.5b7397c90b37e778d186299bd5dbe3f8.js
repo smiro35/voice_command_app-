8d6861b7ccd75c0c9d5faf217fa3a404499353e1
smiro <miiro.shakur@gmail.com>
2020-04-14 00:02:58 -0400

    Updates

---
+++ precache-manifest.5b7397c90b37e778d186299bd5dbe3f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc181dd9a7c837b19e60cbfad349001",
+    "url": "/smiro35/voice_command_app-.git/index.html"
+  },
+  {
+    "revision": "f95259117cc4977da288",
+    "url": "/smiro35/voice_command_app-.git/static/js/2.ed205f49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smiro35/voice_command_app-.git/static/js/2.ed205f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22d9af865aa8142b54c",
+    "url": "/smiro35/voice_command_app-.git/static/js/main.9743aba6.chunk.js"
+  },
+  {
+    "revision": "434cee53a2008b9d121e",
+    "url": "/smiro35/voice_command_app-.git/static/js/runtime-main.9750a66f.js"
+  }
+]);
